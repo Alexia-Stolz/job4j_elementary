310491153290a7ed8dcfc917eb3a9908ad6cb3e0
Alexia-Stolz <i.alexia.stolz@gmail.com>
2025-01-11 22:49:04 +0300

    4.1.4. Псевдокод [#505194]

---
+++ src/main/java/ru/job4j/PseudoCode.java
@@ -0,0 +1,17 @@
+package ru.job4j;
+import java.util.Scanner;
+
+public class PseudoCode {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число ");
+        int number = scanner.nextInt();
+        if (number % 2 == 0) {
+            System.out.println(number + " Это чётное число");
+        }
+        else {
+            System.out.println(number + " Это нечётное число");
+        }
+        scanner.close();
+    }
+}
